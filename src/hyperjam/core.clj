c7425fde1b9a1e963ec668209e81348b8592020f
John Sullivan <john-j-sullivan@outlook.com>
2024-02-25 21:31:27 -0800

    xml

--- src/hyperjam/core.clj
+++ src/hyperjam/core.clj
@@ -1,162 +1,96 @@
 (ns hyperjam.core
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
-            [hickory.render]
+            [clojure.data.xml :as xml]
             [ring.middleware.params]
+            [ring.util.io]
             [ring.adapter.jetty]
             [clojure.spec.alpha :as s]
             [clojure.edn :as edn]))
 
-(s/def ::locator
-  (s/or ::uri uri?
-        ::qualified-symbol qualified-symbol?
-        ::unqualified-symbol (complement qualified-symbol?)))
+(xml/alias-uri 'xh "http://www.w3.org/1999/xhtml")
 
-(defmulti ->resource
-  "Find the resource for the identifier provided."
-  (fn [l]
-    (first (s/conform ::locator l))))
-
-(defmethod ->resource ::uri
+(defn ^:private uri->name
+  "Return the name of the resource denoted by `uri`."
   [uri]
   (-> (.getPath uri)
       (.substring 1)
-      edn/read-string
-      ->resource))
+      edn/read-string))
 
-(defmethod ->resource ::qualified-symbol
+(defn ^:private locate
+  "Find the resource for `name-sym`."
   [name-sym]
-  (find-var name-sym))
+  (cond
+    (qualified-symbol? name-sym) (find-var name-sym)
+    (symbol? name-sym)           (find-ns name-sym)
+    :else                        nil))
 
-(defmethod ->resource ::unqualified-symbol
-  [ns-sym]
-  (find-ns ns-sym))
+(defmulti xhtmlify "Return a representation of `x` as hypertextual data." type)
 
-(defn representation-dispatch
-  "Dispatches for representation multifns."
-  [_resource mime-type & _args]
-  mime-type)
+(defmethod xhtmlify :default
+  [x]
+  [::xh/span {} (print-str x)])
 
-(defmulti ->representation      "Produce the representation for the resource." representation-dispatch)
-(defmulti accept-representation "Send a received representation to the resource." representation-dispatch)
+(defmethod xhtmlify clojure.lang.Var
+  [v]
+  (let [vmeta (meta v)
+        vname (str (-> vmeta :ns ns-name name) "/" (-> vmeta :name name))]
+    [::xh/details {:open true}
+     [::xh/summary {} [::xh/h3 {:style "display:inline;"} vname]]
+     [::xh/span {} (:doc vmeta)]
+     [::xh/form {:action (str "/" vname)
+                 :method "post"}
+      (xhtmlify @v)]]))
 
-(defprotocol URLEncodedParametersRepresentation
-  (accept-urlencodedparams [this params] "Accept a map of parameters from the received representation."))
+(defmethod xhtmlify clojure.lang.IFn
+  [f]
+  [::xh/label {}
+   "Args: " [::xh/input {:name "args" :type "text"}]
+   [::xh/input {:value "Apply" :type "submit"}]])
 
-(extend-protocol URLEncodedParametersRepresentation
-  clojure.lang.Var
-  (accept-urlencodedparams [v params]
-    (accept-urlencodedparams @v params))
-  
-  clojure.lang.Fn
-  (accept-urlencodedparams [f params]
-    (let [args (edn/read-string (str "(" (get params "args") ")"))]
-      (apply f args))))
+(prefer-method xhtmlify clojure.lang.Var clojure.lang.IFn)
 
-(defmethod accept-representation "application/x-www-form-urlencoded"
-  [resource _mime-type request]
-  (accept-urlencodedparams resource (:form-params request)))
+(defn test-fn
+  "test!"
+  [params]
+  (get params "args"))
 
-(defprotocol HiccupRepresentation
-  (->hiccup [this] "Convert resource to hiccup representation."))
-
-(defn ->html
-  "Produce an html representation of `resource`."
-  [resource]
-  (hickory.render/hiccup-to-html
-   [(->hiccup resource)]))
-
-(defmethod ->representation "text/html"
-  [resource _mime-type]
-  (->html resource))
-
-(defn ^:private ns-local-uri
-  "Get the URI of namespace `ns` on the classpath."
-  [ns]
-  (let [local-path (-> ns
-                       ns-name
-                       (#'clojure.core/root-resource)
-                       (.substring 1))]
-    (->> ["cljc" "clj" "cljs"]
-         (map (fn [ext] (io/resource (str local-path "." ext))))
-         (some identity))))
-
-(extend-protocol HiccupRepresentation
-  clojure.lang.Namespace
-  (->hiccup [ns]
-    `[:details {:open "true"}
-      [:summary [:h3 {:style "display:inline;"}
-                 ~(str "Namespace " (ns-name ns))]]
-      ~@(->> (ns-interns ns)
-             vals
-             (filter #(and (instance? clojure.lang.Fn @%)
-                           (not (:private (meta %)))))
-             (map ->hiccup))])
-  
-  clojure.lang.Var
-  (->hiccup [v]
-    (let [vmeta (meta v)
-          vname (str (ns-name (:ns vmeta)) "/" (:name vmeta))]
-      [:details {:open "true"}
-       [:summary [:a {:href (str "/" vname)}
-                  [:h3 {:style "display:inline;"} vname]]]
-       [:section [:p (or (:doc vmeta) "")]]
-       [:form {:method "post" :action (str "/" vname)}
-        (->hiccup @v)]]))
-
-  clojure.lang.IPersistentCollection
-  (->hiccup [x]
-    [:pre (print-str x)])
-  
-  clojure.lang.Fn
-  (->hiccup [f]
-    [:div
-     [:label "Arguments: "
-      [:input {:name "args" :type "text"}]]
-     [:input {:value "Apply" :type "submit"}]]))
+(defn xhtml-doc-root
+  [body]
+  [::xh/html {:xmlns "http://www.w3.org/1999/xhtml"}
+   `[::xh/body {} ~@body]])
 
 (defn ^:private wrap-locate-resource
   [h]
-  (fn [request]
-    (let [uri (-> request :uri java.net.URI.)]
-      (if-let [resource (->resource uri)]
-        (merge (h (merge request {:resource resource}))
-               {:status 200})
-        (merge (h request)
-               {:status 404})))))
-
-(defn ^:private wrap-negotiate-response-type
-  [h]
-  (fn [{:keys [resource] :as request}]
-    (if resource
-      (let [negotiated-type (->> (str/split (get-in request [:headers "accept"]) #",")
-                                 first)
-            response        (h (merge request {:negotiated-type negotiated-type}))]
-        (merge response
-               {:headers (merge (:headers response)
-                                {"Content-Type" negotiated-type})}))
-      (h request))))
+  (fn [{:keys [uri] :as request}]
+    (if-let [resource (locate (uri->name (java.net.URI. uri)))]
+      (merge (h (merge request {:resource resource}))
+             {:status 200})
+      (merge (h request)
+             {:status 404}))))
 
 (def ^:private implemented-http-methods
   #{:get :post})
 
 (defn ^:private wrap-http-method
   [h]
-  (fn [{:keys [negotiated-type resource request-method] :as request}]
+  (fn [{:keys [resource request-method] :as request}]
     (merge (h request)         
-           (when (and negotiated-type resource
+           (when (and resource
                       (contains? implemented-http-methods request-method))
-             (let [received-type (get-in request [:headers "content-type"])]
-               {:body (case request-method
-                        :get  (->representation resource negotiated-type)
-                        :post (-> (accept-representation resource received-type request)
-                                  (->representation negotiated-type))
-                        #_:put  #_(replace-representation resource received-type request))})))))
+             (let [representation (xhtml-doc-root
+                                   [(-> (case request-method
+                                          :get  resource 
+                                          :post (resource (:params request)))
+                                        xhtmlify)])
+                   xml-doc        (-> representation
+                                      xml/sexp-as-element
+                                      (xml/emit-str {:doctype "<!DOCTYPE html>"}))]
+               {:body xml-doc})))))
 
 (def ^:private handler
   (-> (constantly {})
       wrap-http-method
-      wrap-negotiate-response-type
       wrap-locate-resource
       ring.middleware.params/wrap-params))
 
@@ -173,7 +107,7 @@
   (-> [(->hiccup #'->html)]
       hickory.render/hiccup-to-html)
 
-  (meta #'->resource)
+  (meta #'+)
 
   (str 'foo)
 
@@ -183,7 +117,11 @@
 
   (println (keys (methods print-method)))
 
+  (ns-name *ns*)
+
   (with-out-str
     (print-method nil *out*))
+
+  (xhtml-doc-root [(xhtmlify #'test-fn)])
   
   )
